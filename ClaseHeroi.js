a633ae9535eaafd9ad7de6a737610e9da3be95c6
Caio Mendes <caiomendes2212@gmail.com>
2024-10-05 21:16:06 -0300

    commit final

--- ClaseHeroi.js
+++ ClaseHeroi.js
@@ -12,14 +12,24 @@ class Heroi {
 
         if (this.tipo === 'mago') {
             ataque = 'usou magia';
-        }else if (this.tipo === 'guerreiro') {
+        } else if (this.tipo === 'guerreiro') {
             ataque = 'usou espada';
+        } else if (this.tipo === 'monge') {
+            ataque = 'usou artes marciais';
+        } else if (this.tipo === 'ninja') {
+            ataque = 'usou shuriken';
         }
         
-    	console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    	console.log(`O ${this.tipo} ${this.nome} atacou usando ${ataque}`);
     }
 }    
 
-let heroi = new Heroi("Marlin", 1000, "mago");
+let heroi1 = new Heroi("Marlin", 1000, "mago");
+let heroi2 = new Heroi("Ragnar", 33, "guerreiro");
+let heroi3 = new Heroi("Buda", 100000, "monge");
+let heroi4 = new Heroi("Donadello", 15, "ninja");
 
-heroi.atacar();
+heroi1.atacar();
+heroi2.atacar();
+heroi3.atacar();
+heroi4.atacar();
